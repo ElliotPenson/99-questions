23f7b49b506c59789f1fe765e4a73d6892c020f5
ElliotPenson <elliotpenson@gmail.com>
2014-04-08 23:52:44 -0500

    Implement P09

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-99-problems
-===========
+99-questions
+============
 
 [Ninety-Nine Lisp Problems](http://www.ic.unicamp.br/~meidanis/courses/mc336/2006s2/funcional/L-99_Ninety-Nine_Lisp_Problems.html)
 solved in Common Lisp.
---
+++ lists/p09.lisp
@@ -0,0 +1,21 @@
+;;;; p09.lisp
+;;;; Pack consecutive duplicates of list elements into sublists
+
+(defun group (lst)
+  (when lst
+    (if (equal (first lst)
+               (second lst))
+      (cons (first lst)
+            (group (rest lst)))
+      (list (first lst)))))
+
+(defun after-group (lst)
+  (if (equal (first lst)
+             (second lst))
+    (after-group (rest lst))
+    (rest lst)))
+
+(defun pack (lst)
+  (when lst
+    (cons (group lst)
+          (pack (after-group lst)))))
