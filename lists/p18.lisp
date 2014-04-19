13a3f795f90fe27919c7471fb44ededee9885f92
ElliotPenson <elliotpenson@gmail.com>
2014-04-19 17:12:17 -0400

    Implement P18

---
+++ lists/p18.lisp
@@ -0,0 +1,11 @@
+;;;; p18.lisp
+;;;; Given two idices, i and k, the slice is the list containing the elements
+;;;;    between the i'th and k'th element of the original list (both limits
+;;;;    included). Start counting the elements with 1.
+
+(defun slice (lst start end)
+  (loop for item in lst
+        for i from 1
+        if (and (>= i start)
+                (<= i end))
+        collect item))
