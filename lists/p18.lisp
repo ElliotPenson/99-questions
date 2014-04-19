05a56b7dd3c041d3dad718ff3b68538349466520
ElliotPenson <elliotpenson@gmail.com>
2014-04-19 17:16:23 -0400

    Use 99-questions package

--- lists/p18.lisp
+++ lists/p18.lisp
@@ -3,6 +3,8 @@
 ;;;;    between the i'th and k'th element of the original list (both limits
 ;;;;    included). Start counting the elements with 1.
 
+(in-package :99-questions)
+
 (defun slice (lst start end)
   (loop for item in lst
         for i from 1
