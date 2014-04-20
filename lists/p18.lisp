7bf7899c5b2c615a91bf676ae334b0ab4f87e3c7
ElliotPenson <elliotpenson@gmail.com>
2014-04-20 11:39:25 -0400

    Condense P18 description

--- lists/p18.lisp
+++ lists/p18.lisp
@@ -1,7 +1,5 @@
 ;;;; p18.lisp
-;;;; Given two idices, i and k, the slice is the list containing the elements
-;;;;    between the i'th and k'th element of the original list (both limits
-;;;;    included). Start counting the elements with 1.
+;;;; Exract a slice from a list
 
 (in-package :99-questions)
 
