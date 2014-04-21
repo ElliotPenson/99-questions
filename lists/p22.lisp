383b46cb4953f5df4e02eed06f183a41bef86dee
ElliotPenson <elliotpenson@gmail.com>
2014-04-21 13:37:54 -0500

    Use 99-questions package

--- lists/p22.lisp
+++ lists/p22.lisp
@@ -1,6 +1,8 @@
 ;;;; p22.lisp
 ;;;; Create a list containing all integers within a given range
 
+(in-package :99-problems)
+
 (defun range (start end)
   (cond ((= start end) (list start))
         ((< start end) (cons start (range (1+ start) end)))
