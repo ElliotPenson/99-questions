3182d89c3377c49d38ecfd8b8d7b1d009e249cc7
ElliotPenson <elliotpenson@gmail.com>
2014-09-17 22:05:39 -0400

    Use zero-based numbering

--- lists/p20.lisp
+++ lists/p20.lisp
@@ -1,10 +1,10 @@
 ;;;; p20.lisp
-;;;; Remove the k'th element from a list
+;;;; Remove the k'th element from a list (zero-based numbering)
 
 (in-package :99-problems)
 
 (defun remove-at (lst place)
-  (if (= 1 place)
+  (if (zerop place)
     (rest lst)
     (cons (first lst)
           (remove-at (rest lst)
