8bb6c6a61b85565d2821300d471565144c2464a7
Elliot Penson <elliotpenson@gmail.com>
2015-01-18 21:21:34 -0500

    Add p32 to 99-questions package

--- arithmetic/p32.lisp
+++ arithmetic/p32.lisp
@@ -1,5 +1,7 @@
 ;;;; p32.lisp
 ;;;; Determine the greatest common divisor of two positive integer numbers
 
+(in-package :99-questions)
+
 (defun euclid-gcd (x y)
   (if (zerop y) x (euclid-gcd y (mod x y))))
