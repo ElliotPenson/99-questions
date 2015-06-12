4be4b25dee1667d14ecc8f5643c7a8df8dbfcafd
Elliot Penson <elliotpenson@gmail.com>
2015-06-11 21:37:32 -0400

    Add a test for positive numbers in prime-list

--- arithmetic/p31.lisp
+++ arithmetic/p31.lisp
@@ -6,12 +6,13 @@
 (defun primep (n)
   (= n (first (last (prime-list n)))))
 
-;;; Sieve of Eratosthenes
 (defun prime-list (max)
-  (let ((numbers (make-array (1+ max) :initial-element t)))
-    (loop for contender from 2 to max
-       when (aref numbers contender)
-       collect contender
-       do (loop for multiple from contender to max by contender
-	     do (setf (aref numbers multiple) nil)))))
-	 
+  "Evaluates to a list of all prime numbers smaller or equal to max
+   using the Sieve of Eratosthenes algorithm."
+  (when (plusp max)
+    (let ((numbers (make-array (1+ max) :initial-element t)))
+      (loop for contender from 2 to max
+         when (aref numbers contender)
+         collect contender
+         do (loop for multiple from contender to max by contender
+               do (setf (aref numbers multiple) nil))))))
